50829b2b2122d0b41d2fd6ab0116763c745fd784
BergerMarcin <marcin.berger@wp.pl>
2019-10-07 23:58:29 +0200

    My files from classes

---
+++ src/main/java/pl/coderslab/dao/DaoInterface.java
@@ -0,0 +1,4 @@
+package pl.coderslab.dao;
+
+public interface DaoInterface {
+}
---
+++ src/main/java/pl/coderslab/domain/User.java
@@ -0,0 +1,10 @@
+package pl.coderslab.domain;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Long user_group_id;
+
+}
